1180342ab8f90c3b3377523d677d25ca96ceeff7
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-06-12 13:58:34 -0300

    add async fucntion to get and write data

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // DOM
-const searchBox = document.getElementById("search")
+const searchBox = document.getElementById("search");
+const submitSearch = document.getElementById("search-button");
+const cityName = document.getElementById("city-name");
+const todayDate = document.getElementById("today-date");
+const temperature = document.getElementById("temperature");
+const changeUnity = document.getElementById("changeUnity");
 
 
 async function getWeather(cityName) {
@@ -8,4 +13,17 @@ async function getWeather(cityName) {
     const data = await result.json();
     console.log(data)
     return data;
+}
+
+//submit search
+submitSearch.addEventListener("click", async () => {
+    const cityData = await getWeather(searchBox.value);
+    await setCityData(cityData);
+})
+
+// dom functions
+function setCityData(data) {
+    cityName.innerText = data.location.name;
+    todayDate.innerText = data["current"]['last_updated'];
+    temperature.innerText = data["current"]['temp_c']
 }
