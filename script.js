9bdb60c88ab098df9c0e56b20f6e2b9ca9699644
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-06-12 19:52:14 -0300

    add dom function to fill info and data formating functions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchBox = document.getElementById("search");
 const submitSearch = document.getElementById("search-button");
 const cityName = document.getElementById("city-name");
 const todayDate = document.getElementById("today-date");
+const todayTime = document.getElementById("time");
 const temperature = document.getElementById("temperature");
 const changeUnity = document.getElementById("changeUnity");
 
@@ -24,6 +25,33 @@ submitSearch.addEventListener("click", async () => {
 // dom functions
 function setCityData(data) {
     cityName.innerText = data.location.name;
-    todayDate.innerText = data["current"]['last_updated'];
-    temperature.innerText = data["current"]['temp_c']
+    const cityLocalTime = data["location"]["localtime"];
+    let cityLocalTimeHour = cityLocalTime.split(" ");
+    todayDate.innerText = formatDate(cityLocalTimeHour[0])
+    todayTime.innerText = cityLocalTimeHour[1];
+    temperature.innerText = setTemperature(data);
+}
+
+// formating functions
+
+function formatDate(date) {
+    let splitDate = date.split("-");
+    return formatedDate = `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`;
+}
+
+let temperatureUnity = "f";
+
+function setTemperature(temperature) {
+    if (temperatureUnity === "c") {
+        temperatureUnity = "f";
+        return `${temperature["current"]['temp_f']}F`
+        console.log(temperatureUnity)
+    }
+    else {
+        temperatureUnity = "c";
+        console.log(temperatureUnity);
+        return `${temperature["current"]['temp_c']}Cº`;
+    }
+
+
 }
