f72b2c9f58bf1985a000f18e04588f38dfc2ac71
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-06-12 11:40:14 -0300

    add search bar and initial structure for weather display

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function test() {
-    alert("It works")
+async function getWeather(cityName) {
+    const URL = `http://api.weatherapi.com/v1/current.json?key=a65ccd428bd445c9af4172452230706&q=${cityName}&aqi=no`
+    const result = await fetch(URL);
+    const data = await result.json();
+    console.log(data)
+    return data;
 }
-
-test()
